da226ace14b2f8da3c6c4a4b46acb9975c5216ef
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-10-02 18:50:23 -0500

    TASK: react-native-dropdown-picker library added

--- app/screen/AttendeesScreen.tsx
+++ app/screen/AttendeesScreen.tsx
@@ -1,11 +1,58 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+import DropDownPicker from "react-native-dropdown-picker";
+
 import { globalStyles } from "./globalStyles";
 
 export default function AttendeesScreen() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  // TODO: Get Conferences from DB
+  const [items, setItems] = useState([
+    { label: "Apple Conference XVI", value: "apple-xvi" },
+    { label: "CodeWave IX", value: "codewave-ix" },
+    { label: "AppCon X", value: "appcon-x" },
+    { label: "MobileTech XIII", value: "mobiletech-xiii" },
+    { label: "DevSync VII", value: "devsync-vii" },
+    { label: "AppTalk I", value: "apptalk-i" },
+    { label: "MobiXpo XII", value: "mobixpo-xii" },
+  ]);
+
   return (
-    <View style={globalStyles.container}>
-      <Text style={globalStyles.title}>List of registered event attendees</Text>
+    <View style={attendeesStyles.container}>
+      <Text style={globalStyles.title}>Conference Attendees</Text>
+      <Text style={attendeesStyles.description}>
+        Here you can list the conferences attendees
+      </Text>
+
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
+
+      <Text style={attendeesStyles.attendees_list}>
+        {value ? value : "Here will be the attendees"}
+      </Text>
     </View>
   );
 }
+
+const attendeesStyles = StyleSheet.create({
+  container: {
+    padding: 32,
+  },
+  description: {
+    fontSize: 20,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  attendees_list: {
+    marginTop: 24,
+    fontSize:20
+  },
+});
